ecd9104aa53f5e00daefa18a73203c0169324acd
Kyllerium Admin <admin@kyllerium.com>
2026-05-19 14:30:18 -0600

    Fix: Restrict outputFileTracingIncludes to prevent Vercel 250MB limit error

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['c2pa-node', 'pdfkit', 'sharp', 'piexifjs', 'crypto', 'child_process'],
   outputFileTracingIncludes: {
     '/api/**/*': [
-      './node_modules/**/*.wasm',
-      './node_modules/**/*.node',
-      './node_modules/c2pa-node/**/*',
-      './node_modules/@contentauth/c2pa-node/**/*'
+      './node_modules/c2pa-node/dist/**/*.node',
+      './node_modules/c2pa-node/tests/**/*'
     ]
   }
 };
